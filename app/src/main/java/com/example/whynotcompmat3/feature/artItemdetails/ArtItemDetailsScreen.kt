2ed964fbb3bd3386ffe1db224b3df83f710976d9
Blanka <blanka@blanka>
2022-06-03 13:43:37 +0200

    adjustments to art item details screen

--- app/src/main/java/com/example/whynotcompmat3/feature/artItemdetails/ArtItemDetailsScreen.kt
+++ app/src/main/java/com/example/whynotcompmat3/feature/artItemdetails/ArtItemDetailsScreen.kt
@@ -1,6 +1,5 @@
 package com.example.whynotcompmat3.feature.artItemdetails
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -9,6 +8,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.AsyncImage
 import com.example.whynotcompmat3.R
@@ -37,31 +37,53 @@ fun ArtItemDetailsScreen(imageId: String) {
 fun ArtItemDetailsComposable(art: ArtItem) {
 
     Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
-            .padding(vertical = dimensionResource(id = R.dimen.padding_common_big))
+            .padding(
+                vertical = dimensionResource(id = R.dimen.padding_common_big),
+                horizontal = dimensionResource(id = R.dimen.padding_common)
+            )
             .fillMaxSize()
     ) {
         Row(
             horizontalArrangement = Arrangement.Center,
             modifier = Modifier
                 .fillMaxWidth()
-        ) {}
-        AsyncImage(
-            model = art.imageUrl,
-            contentDescription = stringResource(id = R.string.painting_description, art.title),
-            modifier = Modifier.padding(bottom = dimensionResource(id = R.dimen.padding_common_small))
+        ) {
+            AsyncImage(
+                model = art.imageUrl,
+                contentDescription = stringResource(id = R.string.painting_description, art.title),
+                modifier = Modifier.padding(bottom = dimensionResource(id = R.dimen.padding_common))
+            )
+        }
+        ArtistDataSet(stringResource(id = R.string.art_item_title), art.title)
+        ArtistDataSet(stringResource(id = R.string.art_item_artist_name), art.artistName)
+        ArtistDataSet(stringResource(id = R.string.art_item_place_of_origin), art.placeOfOrigin)
+        ArtistDataSet(
+            stringResource(id = R.string.art_item_style),
+            if (art.styleTitle.isNullOrEmpty()) stringResource(
+                id = R.string.art_item_no_data
+            ) else art.styleTitle
+        )
+
+    }
+
+}
+
+@Composable
+fun ArtistDataSet(title: String, data: String) {
+    Column(
+        modifier = Modifier.padding(bottom = dimensionResource(id = R.dimen.padding_common_small))
+    ) {
+        Text(
+            title,
+            style = MaterialTheme.typography.bodyMedium,
+        )
+        Text(
+            data,
+            style = MaterialTheme.typography.bodyLarge,
+            fontWeight = FontWeight.Bold
         )
     }
-    Text(
-        art.title,
-        style = MaterialTheme.typography.bodyLarge,
-        modifier = Modifier.padding(bottom = dimensionResource(id = R.dimen.padding_common_small))
-    )
-    Text(
-        art.artistName,
-        style = MaterialTheme.typography.bodyMedium,
-    )
 }
 
 
